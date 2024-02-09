1ca7cf2cc2df8fdd9ebc1cf5148e90a3106573a8
conduition <conduition@proton.me>
2024-02-09 08:24:40 +0000

    randomness feature

--- src/lib.rs
+++ src/lib.rs
@@ -10,3 +10,11 @@ pub use sharing::*;
 // Re-Exports
 pub use secp;
 pub use sha2;
+
+#[cfg(feature = "rand")]
+pub fn random_coefficients<R: rand::RngCore + rand::CryptoRng>(
+    rng: &mut R,
+    n: usize,
+) -> Vec<secp::MaybeScalar> {
+    (0..n).map(|_| secp::Scalar::random(rng).into()).collect()
+}
